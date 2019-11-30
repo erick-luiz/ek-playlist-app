72418b9b56344735d37b7e74ce2292e4b24a2d5a
Erick Luiz Fonseca Lopes <902139@lojasrenner.com.br>
2019-11-30 01:37:26 -0300

    Feat Start Project

---
+++ src/test/java/com/ekapp/playlist/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekapp.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
